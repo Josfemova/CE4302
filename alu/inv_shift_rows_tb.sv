c9564ad90554ed94af880c85a253decf209ebe6b
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2024-09-25 23:18:46 -0600

    Actualización de Shift Rows y su inverso

--- alu/inv_shift_rows_tb.sv
+++ alu/inv_shift_rows_tb.sv
@@ -16,14 +16,14 @@ module inv_shift_rows_tb;
         #10;
 		  // Pruebas inversas de shift rows tb
 		  // Prueba 1
-        state_in = 128'h23456701cdef89ab6701234589abcdef;
+        state_in = 128'h8923cd6701ab45ef8923cd6701ab45ef;
         #10; 
 
         $display("Input:  %h", state_in);
         $display("Output: %h", state_out);
 
         // Prueba 2
-        state_in = 128'hcdef01ab6789234501abcdef23456789;
+        state_in = 128'h23cd6701ab45ef8923cd6701ab45ef89;
         #10; 
 
         $display("Input:  %h", state_in);
