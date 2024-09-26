39773f1711e3743dff0016505fe1a4659b480b1d
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2024-09-25 23:18:46 -0600

    Actualización de Shift Rows y su inverso

--- alu/shift_rows.sv
+++ alu/shift_rows.sv
@@ -3,19 +3,26 @@ module shift_rows (
     output logic [127:0] state_out
 );
     // Variables temporales para cada fila de la matriz 4x4 
-    logic [31:0] row [0:3];
+    logic [31:0] column [0:3];
 
-    always_comb begin
-        // Dividir el estado en filas de 32 bits (4 bytes por fila)
-        row[0] = state_in[31:0];    
-        row[1] = state_in[63:32];   
-        row[2] = state_in[95:64];   
-        row[3] = state_in[127:96];  
+    always @(*) begin
+        // Dividir el estado en columnas de 32 bits (4 bytes por columna) 
+        column[0] = state_in[31:0];
+        column[1] = state_in[63:32];
+        column[2] = state_in[95:64];
+        column[3] = state_in[127:96];
 
-        // Aplicar el desplazamiento circular a cada fila
-        state_out[31:0]    = row[0];                          // Primera fila (sin cambios)
-        state_out[63:32]   = {row[1][7:0], row[1][31:8]};     // Segunda fila (rotar 1 byte a la izquierda)
-        state_out[95:64]   = {row[2][15:0], row[2][31:16]};   // Tercera fila (rotar 2 bytes a la izquierda)
-        state_out[127:96]  = {row[3][23:0], row[3][31:24]};   // Cuarta fila (rotar 3 bytes a la izquierda)
+        state_out[31:0] = { 
+             column[3][31:24], column[2][23:16], column[1][15:8], column[0][7:0]
+        };
+        state_out[63:32] = {
+            column[0][31:24], column[3][23:16], column[2][15:8], column[1][7:0]
+        };
+        state_out[95:64] = {
+            column[1][31:24], column[0][23:16], column[3][15:8], column[2][7:0]
+        };
+        state_out[127:96] = {
+            column[2][31:24], column[1][23:16], column[0][15:8], column[3][7:0]
+        };
     end
 endmodule
