3be3978b7d1f69331871d3d17f6070869ec3cd5e
josfemova <josfemova@gmail.com>
2024-09-27 01:29:02 -0600

    decrypt vectorial en asm funcional :3

--- proyecto1/hardware/rtl/core/aes_ops/inv_shift_rows.sv
+++ proyecto1/hardware/rtl/core/aes_ops/inv_shift_rows.sv
@@ -13,16 +13,16 @@ module inv_shift_rows (
         column[3] = state_in[127:96];
 
         state_out[31:0] = { 
-            column[3][31:24], column[2][23:16], column[1][15:8], column[0][7:0] 
+            column[1][31:24], column[2][23:16], column[3][15:8], column[0][7:0] 
         }; 
         state_out[63:32] = { 
-            column[2][31:24], column[1][23:16], column[0][15:8], column[3][7:0] 
+            column[2][31:24], column[3][23:16], column[0][15:8], column[1][7:0] 
         }; 
         state_out[95:64] = { 
-            column[1][31:24], column[0][23:16], column[3][15:8], column[2][7:0] 
+            column[3][31:24], column[0][23:16], column[1][15:8], column[2][7:0] 
         }; 
         state_out[127:96] = { 
-            column[0][31:24], column[3][23:16], column[2][15:8], column[1][7:0] 
+            column[0][31:24], column[1][23:16], column[2][15:8], column[3][7:0] 
         }; 
     end
 endmodule
