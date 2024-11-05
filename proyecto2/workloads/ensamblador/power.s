8d33e48674ea515964b6fe72db64f20be27247f9
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2024-11-04 21:54:54 -0600

    Correcciones al ensamblador sequencial de potencia n de 2

--- proyecto2/workloads/ensamblador/power.s
+++ proyecto2/workloads/ensamblador/power.s
@@ -1,18 +1,20 @@
 .section .text
 .global _start
 _start:
-    MOV r0, #4
-    MOV r1, #1
-    MOV r2, r0
+    MOV r0, #4  @ n potencia de 2
+    MOV r1, #1  @ inicializacion del resultado
+    MOV r2, r0  @ inicializacion del contador de iteraciones
 
 loop:
-    MOV r3, #2
-    MUL r1, r3, r1
-    SUB r2, r2, #1
+    MOV r3, #2        @ equivalente a LOAD
+    MUL r1, r3, r1    
+    SUB r2, r2, #1    @ equivalente a DEC
 
-    CMP r2, #0
+    CMP r2, #0        @ equivalente a JNE
     BNE loop
 
 end:
-    LDR r3, =0x100
-    STR r1, [r3]
+    @ syscall de exit
+    MOV r7, #1        
+    MOV r0, #0        
+    SVC #0            
